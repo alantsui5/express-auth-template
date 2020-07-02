96f49122f812200a5729b386ff9058dd1ac3bc0e
alantsuihoming <4079tsui@gmail.com>
2020-07-03 01:31:36 +0800

    Add back JWT after creating admin

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -6,7 +6,7 @@ import { Router } from "express";
   const router = Router();
 
   //Get all users
-  router.get("/" /*, [checkJwt, checkRole(["ADMIN"])] */, UserController.listAll);
+  router.get("/" , [checkJwt, checkRole(["ADMIN"])] , UserController.listAll);
 
   // Get one user
   router.get(
@@ -16,7 +16,7 @@ import { Router } from "express";
   );
 
   //Create a new user
-  router.post("/", /*[checkJwt, checkRole(["ADMIN"])],*/ UserController.newUser);
+  router.post("/", [checkJwt, checkRole(["ADMIN"])], UserController.newUser);
 
   //Edit one user
   router.patch(
