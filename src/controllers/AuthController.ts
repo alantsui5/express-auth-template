3a5e33d1c6cf6a1137a5ba625ca4ee8727e05145
alantsuihoming <4079tsui@gmail.com>
2020-07-03 01:04:54 +0800

    Add debug log

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -10,6 +10,7 @@ class AuthController {
   static login = async (req: Request, res: Response) => {
     //Check if username and password are set
     let { username, password } = req.body;
+    console.log(`${username} ${password}`)
     if (!(username && password)) {
       res.status(400).send();
     }
