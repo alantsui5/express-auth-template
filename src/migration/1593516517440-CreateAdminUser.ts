d373d4e578cb34dda4bbc629debfd77f7068866b
alantsuihoming <4079tsui@gmail.com>
2020-07-03 01:16:33 +0800

    Change Migration file

--- src/migration/1593516517440-CreateAdminUser.ts
+++ src/migration/1593516517440-CreateAdminUser.ts
@@ -1,8 +1,16 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner, getRepository } from "typeorm";
+import { User } from "../entity/User";
 
 export class CreateAdminUser1593516517440 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        let user = new User();
+        user.username = "admin";
+        user.password = "admin";
+        user.hashPassword();
+        user.role = "ADMIN";
+        const userRepository = getRepository(User);
+        await userRepository.save(user);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
