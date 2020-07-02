cb69a09f1de9cf377edc3eea790998a34c263315
alantsuihoming <4079tsui@gmail.com>
2020-07-03 00:37:27 +0800

    Change Port

--- src/index.ts
+++ src/index.ts
@@ -40,7 +40,7 @@ createConnection({
     //Set all routes from routes folder
     app.use("/", routes);
 
-    app.listen(3000, () => {
+    app.listen(process.env.PORT, () => {
       console.log("Server started on port 3000!");
     });
   })
