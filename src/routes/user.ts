79fbe63d54ce8101b8d8bc72ea695d0da3bc9233
alantsuihoming <4079tsui@gmail.com>
2020-07-03 01:37:47 +0800

    Debug jwt

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -6,7 +6,7 @@ import { Router } from "express";
   const router = Router();
 
   //Get all users
-  router.get("/" , [/*checkJwt*/, checkRole(["ADMIN"])] , UserController.listAll);
+  router.get("/" , [/*checkJwt,*/ checkRole(["ADMIN"])] , UserController.listAll);
 
   // Get one user
   router.get(
