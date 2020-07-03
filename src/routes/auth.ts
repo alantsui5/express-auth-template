99f15300c751518b7e73e037b392d4b86624be41
alantsuihoming <4079tsui@gmail.com>
2020-07-03 10:41:53 +0800

    Add register route

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import AuthController from "../controllers/AuthController";
 import { checkJwt } from "../middlewares/checkJwt";
+import UserController from "../controllers/UserController";
 
 const router = Router();
 //Login route
@@ -8,5 +9,9 @@ router.post("/login", AuthController.login);
 
 //Change my password
 router.post("/change-password", [checkJwt], AuthController.changePassword);
+
+//Create a new user
+router.post("/register", UserController.newUser);
+
 
 export default router;
